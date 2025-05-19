7b66a69e41892962f1b4b2cc837e0719b2b0210a
Benjamin Haag <Benjamin.Haag@usda.gov>
2025-05-19 15:51:37 -0400

    Added logic to Accession query.

--- USDA.ARS.NPGS/USDA.ARS.GRINGlobal.Domain/Services/AccessionRepository.cs
+++ USDA.ARS.NPGS/USDA.ARS.GRINGlobal.Domain/Services/AccessionRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 using USDA.ARS.GRINGlobal.Data.Models;
 using USDA.ARS.GRINGlobal.Domain.Models;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace USDA.ARS.GRINGlobal.Domain.Services
 {
@@ -31,6 +33,27 @@ namespace USDA.ARS.GRINGlobal.Domain.Services
 
         public async Task<IEnumerable<AccessionDTO>> GetAccessionsByCriteriaAsync(AccessionCriteriaDTO criteria)
         {
+            var sql = new StringBuilder("SELECT Accession, Plant_Name, DOI, taxonomy_species_id, Origin, Genebank, Image, Availability, Received, Source_Type, Source_Date, Collection_Site, Improvement_Level InitialReceivedDate FROM gringlobal].dbo].vw_GRINGlobal_Accession_Summary]");
+
+            var parameters = new List<SqlParameter>();
+
+            if (!string.IsNullOrEmpty(criteria.accession_identifier))
+            {
+                sql.Append(" AND Name LIKE '%' + @name + '%'");
+                parameters.Add(new SqlParameter("@accession_identifier", criteria.accession_identifier));
+            }
+
+            //if (minAge.HasValue)
+            //{
+            //    sql.Append(" AND Age >= @minAge");
+            //    parameters.Add(new SqlParameter("@minAge", minAge.Value));
+            //}
+
+            //// Execute query
+            //var results = await context.Database
+            //    .SqlQueryRaw<MyDto>(sql.ToString(), parameters.ToArray())
+            //    .ToListAsync();
+
             var accessions = _context.Accessions.AsQueryable();
 
             if (!String.IsNullOrEmpty(criteria.accession_identifier))
@@ -90,7 +113,7 @@ namespace USDA.ARS.GRINGlobal.Domain.Services
 
         public async Task<IEnumerable<TabularReportItemDTO>> GetReportSummaryDiversityAsync()
         {
-            var report = await _context.Database.SqlQueryRaw<TabularReportItemDTO>("SELECT title_field, total_field FROM vw_GRINGlobal_Rpt_Summary_Diversity ORDER BY title_field").ToListAsync();
+            var report = await _context.Database.SqlQueryRaw<TabularReportItemDTO>("SELECT Accession, total_field FROM vw_GRINGlobal_Rpt_Summary_Diversity ORDER BY title_field").ToListAsync();
             return report;
         }
 
